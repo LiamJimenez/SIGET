de97272367cf9dea6bc192db3b1414d9b117391c
LiamJimenez <liammarcos45@gmail.com>
2024-08-01 15:41:23 -0400

    Agregar Login y Registro

--- SIGET.DataAccess/Data/ApplicationDbContext.cs
+++ SIGET.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SIGET.Models;
 
 namespace SIGET.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,6 +16,9 @@ namespace SIGET.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Colaboradores>().HasData(
                 new Colaboradores
                 {
